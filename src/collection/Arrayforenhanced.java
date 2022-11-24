5074396ccfb0dda4b07fb9e9f1aca82cd8bfeb4a
sufiyannagaralli <sufiyannagaralli99@gmail.com>
2022-11-24 20:21:49 +0530

    This code changer is done by asian guy

--- src/collection/Arrayforenhanced.java
+++ src/collection/Arrayforenhanced.java
@@ -6,9 +6,11 @@ public class Arrayforenhanced {
 
 	public static void main(String[] args) {
 		int[] marks = { 200, 500, 400, 2 };
-		int[] marks1 = { 200, 500, 400,2};
+		int[] marks1 = { 200, 500, 400, 2 };
 		{
 			System.out.println(Arrays.equals(marks, marks1));
+			System.out.println("My name is Sufiyan Nagaralli");
+			System.out.println("Belive or not im sufiyan nagaralli");
 
 		}
 
